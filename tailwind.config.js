2e60f374357c5853b9b6f11f0aba49d505a12ea7
Alejandro Chinea Toledo <cachinea@uoc.edu>
2024-01-07 22:30:52 +0100

    Entrega

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        color_uoc: '#000078',
+        primary: '#FF8E23',
+        secondary: '#10B5AB'
+      },
+    },
+  },
+  plugins: [
+
+  ],
+}
+
